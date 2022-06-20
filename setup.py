106951093e7961ff072c6eb0bf5a8ee0f9757eeb
kabero <q4bero@gmail.com>
2022-06-21 03:02:08 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ console_scripts = [
 
 setup(
     name='chinolab',
-    version='0.0.3',
+    version='0.0.4',
     packages=packages,
     install_requires=install_requires,
     entry_points={'console_scripts': console_scripts}
